a4b806f53d85e0611a5babf1a9aeeaeafd0ac97a
FaberVitale <faber.vitale@gmail.com>
2019-06-15 20:40:58 +0200

    Updates

---
+++ precache-manifest.bcd7b01525d52f9090720b26ff238aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec8de9f771d41613af039b9dd01b79c",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "410ed8b1a72290ddbe24",
+    "url": "/tictactoe/static/css/main.f3c67501.chunk.css"
+  },
+  {
+    "revision": "cca66356d2b2f2b57707",
+    "url": "/tictactoe/static/js/2.63368133.chunk.js"
+  },
+  {
+    "revision": "05f859991566a98f3d8a",
+    "url": "/tictactoe/static/js/3.5c80739b.chunk.js"
+  },
+  {
+    "revision": "410ed8b1a72290ddbe24",
+    "url": "/tictactoe/static/js/main.9d867820.chunk.js"
+  },
+  {
+    "revision": "98e277d123f2e35b7c69",
+    "url": "/tictactoe/static/js/runtime~main.c74aeef1.js"
+  }
+]);
